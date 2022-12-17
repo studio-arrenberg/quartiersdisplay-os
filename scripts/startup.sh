7dd792d2e01d0256465d23a1508f7d1b06d8f296
camilomartins <66945701+camilomartins@users.noreply.github.com>
2022-12-17 19:00:05 +0100

    Create startup.sh

---
+++ scripts/startup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Starting nodejs Server"
+git fetch
+git pull
+echo "Git Update erledigt"
+npm Install
+npm run dev
+echo "Next environment running"
+
+echo "Git Update erledigt"
